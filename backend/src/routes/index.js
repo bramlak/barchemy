0b167db6ec58f2751d668cf87da778fbd190c739
bramlak <kalmarbalint97@gmail.com>
2025-10-13 17:50:39 +0200

    feature(ingredient): create top-level route for ingredient

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import recipeRoutes from '../modules/recipe/recipe.routes.js';
-
+import ingredientRoutes from '../modules/ingredient/ingredient.routes.js'
 const router = Router();
 router.use('/recipe', recipeRoutes);
+router.use('/ingredient', ingredientRoutes);
 
 
 export default router;
